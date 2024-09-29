06a24f4c16f8ba7966753ac71765ba24dfdcb404
Yosha <jayrosellosa91@gmail.com>
2024-09-30 01:04:03 +0800

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAA IM PUSSSHIINNN

--- app/src/main/java/com/example/g2calc/calc.java
+++ app/src/main/java/com/example/g2calc/calc.java
@@ -3,16 +3,15 @@ import android.annotation.SuppressLint;
 import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.view.MotionEvent;
-import android.view.View;
-import android.view.Window;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
-;
 public class calc extends AppCompatActivity {
 
     Button button, button2, button3, button4;
@@ -41,55 +40,83 @@ public class calc extends AppCompatActivity {
 
 //Animation Buttons Only
 
-     ;   android_button = AnimationUtils.loadAnimation(this, R.anim.android_button);
+        android_button = AnimationUtils.loadAnimation(this, R.anim.android_button);
         android_button2 = AnimationUtils.loadAnimation(this, R.anim.android_button2);
 
-        button.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent motionEvent) {
-                if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
-                    button.startAnimation(android_button);
-                } else if (motionEvent.getAction() == MotionEvent.ACTION_UP) {
-                    button.startAnimation(android_button2);
-                }
-                return true;
+        button.setOnTouchListener((v, motionEvent) -> {
+            if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+                button.startAnimation(android_button);
+
+                EditText input1 = findViewById(R.id.editTextNumber4);
+                EditText input2 = findViewById(R.id.editTextNumber5);
+                TextView tot = findViewById(R.id.editTextNumber6);
+                Double num1 = Double.parseDouble(input1.getText().toString());
+                Double num2 = Double.parseDouble(input2.getText().toString());
+                Double total =  num1 + num2;
+
+                tot.setText(String.valueOf(total));
+
+            } else if (motionEvent.getAction() == MotionEvent.ACTION_UP) {
+                button.startAnimation(android_button2);
             }
+            return true;
         });
 
-        button2.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent motionEvent) {
-                if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
-                    button2.startAnimation(android_button);
-                } else if (motionEvent.getAction() == MotionEvent.ACTION_UP) {
-                    button2.startAnimation(android_button2);
-                }
-                return true;
+        button2.setOnTouchListener((v, motionEvent) -> {
+            if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+                button2.startAnimation(android_button);
+
+                EditText input1 = findViewById(R.id.editTextNumber4);
+                EditText input2 = findViewById(R.id.editTextNumber5);
+                TextView tot = findViewById(R.id.editTextNumber6);
+                Double num1 = Double.parseDouble(input1.getText().toString());
+                Double num2 = Double.parseDouble(input2.getText().toString());
+                Double total =  num1 - num2;
+
+                tot.setText(String.valueOf(total));
+
+            } else if (motionEvent.getAction() == MotionEvent.ACTION_UP) {
+                button2.startAnimation(android_button2);
             }
+            return true;
         });
 
-        button3.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent motionEvent) {
-                if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
-                    button3.startAnimation(android_button);
-                } else if (motionEvent.getAction() == MotionEvent.ACTION_UP) {
-                    button3.startAnimation(android_button2);
-                }
-                return true;
+        button3.setOnTouchListener((v, motionEvent) -> {
+            if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+                button3.startAnimation(android_button);
+
+                EditText input1 = findViewById(R.id.editTextNumber4);
+                EditText input2 = findViewById(R.id.editTextNumber5);
+                TextView tot = findViewById(R.id.editTextNumber6);
+                Double num1 = Double.parseDouble(input1.getText().toString());
+                Double num2 = Double.parseDouble(input2.getText().toString());
+                Double total =  num1 * num2;
+
+                tot.setText(String.valueOf(total));
+
+            } else if (motionEvent.getAction() == MotionEvent.ACTION_UP) {
+                button3.startAnimation(android_button2);
             }
+            return true;
         });
 
-        button4.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent motionEvent) {
-                if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
-                    button4.startAnimation(android_button);
-                } else if (motionEvent.getAction() == MotionEvent.ACTION_UP) {
-                    button4.startAnimation(android_button2);
-                }
-                return true;
+        button4.setOnTouchListener((v, motionEvent) -> {
+            if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+                button4.startAnimation(android_button);
+
+                EditText input1 = findViewById(R.id.editTextNumber4);
+                EditText input2 = findViewById(R.id.editTextNumber5);
+                TextView tot = findViewById(R.id.editTextNumber6);
+                Double num1 = Double.parseDouble(input1.getText().toString());
+                Double num2 = Double.parseDouble(input2.getText().toString());
+                Double total =  num1 / num2;
+
+                tot.setText(String.valueOf(total));
+
+            } else if (motionEvent.getAction() == MotionEvent.ACTION_UP) {
+                button4.startAnimation(android_button2);
             }
+            return true;
         });
 
     }
