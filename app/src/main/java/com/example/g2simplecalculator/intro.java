765be7fe407b4a2de177ec7f0738292f114b68c9
Vcint27 <vhencintgimena27@gmail.com>
2024-10-02 01:18:25 +0800

    Initial Comment

---
+++ app/src/main/java/com/example/g2simplecalculator/intro.java
@@ -0,0 +1,36 @@
+package com.example.g2simplecalculator;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class intro extends AppCompatActivity {
+
+    public static int SPLASH_TIMER = 1700;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.intro);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(intro.this, simplecalc.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_TIMER);
+
+
+
+
+    }
+}
---
+++ app/src/main/java/com/example/g2simplecalculator/simplecalc.java
@@ -0,0 +1,32 @@
+package com.example.g2simplecalculator;
+
+import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class simplecalc extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.simplecalc);
+        ConstraintLayout constraintLayout = findViewById(R.id.main);
+        AnimationDrawable animationDrawable = (AnimationDrawable) constraintLayout.getBackground();
+        animationDrawable.setEnterFadeDuration(1000);
+        animationDrawable.setExitFadeDuration(2500);
+        animationDrawable.start();
+
+
+
+    }
+}
