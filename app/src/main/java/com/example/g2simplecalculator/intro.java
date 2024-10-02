19a998d6ec818144c6fd55f04bf8d7d2da86acad
Trabby-Patty <12agoncillo.traballojhonwazel@gmail.com>
2024-10-02 10:23:45 +0800

    aww

--- app/src/main/java/com/example/g2simplecalculator/intro.java
+++ app/src/main/java/com/example/g2simplecalculator/intro.java
@@ -10,7 +10,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.example.g2calc.R;
+import com.example.g2simplecalculator.simplecalc.R;
 
 public class intro extends AppCompatActivity {
 
